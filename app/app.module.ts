d056b0128ef667a5bde1d4d31b073a80f5eb0a63
Jundong Liao <jxljdx@gmail.com>
2016-12-25 20:27:47 -0800

    Added basic template to display Hero info
    What to learn:
    - Basic class definition and declaration
    - multi-line html template using ``
    -  2-way data binding, [(ngModel)]="hero.name"

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// For using [(NgModule)] in <Input>
+import { FormsModule }   from '@angular/forms';
+
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
